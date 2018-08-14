88fd2730f3e8a372b89fc963ccb766100f3f7fad
ShairaT <ayelenmaite2000@gmail.com>
2018-08-14 20:54:47 -0300

    Flask

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import pymysql
 import os
 
 
@@ -70,28 +69,29 @@ while(loop):
                 Dueno().DeleteDueno(dueno_a_borrar)
 
             elif opcion_duenos == "4":
-                os.system('cls')
-                Menu.MenuSelects()
-                opcion_select_duenos = input("Elija la funcion: ")
-
-                if opcion_select_duenos == "1":
+                while(loop):
                     os.system('cls')
-                    print("SELECT ALL DUENOS")
-                    listaDuenos = Dueno.SeleccionarDuenos()
-                    for item in listaDuenos:
-                        print(item.id, item.nombre, item.apellido, item.DNI, item.telefono)
+                    Menu.MenuSelects()
+                    opcion_select_duenos = input("Elija la funcion: ")
 
-                elif opcion_select_duenos == "2":
-                    os.system('cls')
-                    print("SELECT POR ID DUENOS")
-                    dueno_a_seleccionar = input("Ingrese el dueno a seleccionar")
-                    dueno = Dueno.SeleccionarDuenosPorID(dueno_a_seleccionar)
-                    print(str(dueno.id), dueno.nombre, dueno.apellido, str(dueno.DNI), dueno.telefono)
-                elif opcion_select_duenos == "3":
-                    break
-                elif opcion_select_duenos == "4":
-                    loop = False
-                else: equivocacion_select_dueno = input("Opcion incorrecta")
+                    if opcion_select_duenos == "1":
+                        os.system('cls')
+                        print("SELECT ALL DUENOS")
+                        listaDuenos = Dueno.SeleccionarDuenos()
+                        for item in listaDuenos:
+                            print(item.id, item.nombre, item.apellido, item.DNI, item.telefono)
+
+                    elif opcion_select_duenos == "2":
+                        os.system('cls')
+                        print("SELECT POR ID DUENOS")
+                        dueno_a_seleccionar = input("Ingrese el dueno a seleccionar")
+                        dueno = Dueno.SeleccionarDuenosPorID(dueno_a_seleccionar)
+                        print(str(dueno.id), dueno.nombre, dueno.apellido, str(dueno.DNI), dueno.telefono)
+                    elif opcion_select_duenos == "3":
+                        break
+                    elif opcion_select_duenos == "4":
+                        loop = False
+                    else: equivocacion_select_dueno = input("Opcion incorrecta")
             elif opcion_duenos == "5":
                 break
             elif opcion_duenos == "6":
@@ -141,28 +141,29 @@ while(loop):
                 Veterinario.DeleteVeterinario(veterinario_a_borrar)
 
             elif opcion_veterinarios == "4":
-                os.system('cls')
-                Menu.MenuSelects()
-                opcion_select_veterinarios = input("Elija la funcion: ")
-
-                if opcion_select_veterinarios == "1":
+                while(loop):
                     os.system('cls')
-                    print("SELECT ALL VETERINARIOS")
-                    listaveterinarios = Veterinario.SeleccionarVeterinarios()
-                    for item in listaveterinarios:
-                        print(item.id, item.nombre, item.apellido, item.DNI, item.sueldo)
+                    Menu.MenuSelects()
+                    opcion_select_veterinarios = input("Elija la funcion: ")
 
-                elif opcion_select_veterinarios == "2":
-                    os.system('cls')
-                    print("SELECT POR ID VETERINARIOS")
-                    veterinario_a_seleccionar = input("Ingrese el veterinario a seleccionar")
-                    veterinario = Veterinario.SeleccionarVeterinariosPorID(veterinario_a_seleccionar)
-                    print(str(veterinario.id), veterinario.nombre, veterinario.apellido, str(veterinario.DNI), veterinario.sueldo)
-                elif opcion_select_veterinarios == "3":
-                    break
-                elif opcion_select_veterinarios == "4":
-                    loop = False
-                else:   equivocacion_select_veterinario = input("Opcion incorrecta")
+                    if opcion_select_veterinarios == "1":
+                        os.system('cls')
+                        print("SELECT ALL VETERINARIOS")
+                        listaveterinarios = Veterinario.SeleccionarVeterinarios()
+                        for item in listaveterinarios:
+                            print(item.id, item.nombre, item.apellido, item.DNI, item.sueldo)
+
+                    elif opcion_select_veterinarios == "2":
+                        os.system('cls')
+                        print("SELECT POR ID VETERINARIOS")
+                        veterinario_a_seleccionar = input("Ingrese el veterinario a seleccionar")
+                        veterinario = Veterinario.SeleccionarVeterinariosPorID(veterinario_a_seleccionar)
+                        print(str(veterinario.id), veterinario.nombre, veterinario.apellido, str(veterinario.DNI), veterinario.sueldo)
+                    elif opcion_select_veterinarios == "3":
+                        break
+                    elif opcion_select_veterinarios == "4":
+                        loop = False
+                    else:   equivocacion_select_veterinario = input("Opcion incorrecta")
             elif opcion_veterinarios == "5":
                 break
             elif opcion_veterinarios == "6":
@@ -202,29 +203,30 @@ while(loop):
                 mascota_a_borrar = input("Ingrese la mascota a borrar: ")
                 Mascota.DeleteMascota(mascota_a_borrar)
             elif opcion_mascotas == "4":
-                os.system('cls')
-                Menu.MenuSelects()
-                opcion_select_mascotas = input("Elija la funcion: ")
-
-                if opcion_select_mascotas == "1":
+                while(loop):
                     os.system('cls')
-                    print("SELECT ALL MASCOTAS")
-                    listamascotas = Mascota.SelectMascota()
-                    for item in listamascotas:
-                        print(item.id, item.nombre, item.tipo, item.dueno.id)
+                    Menu.MenuSelects()
+                    opcion_select_mascotas = input("Elija la funcion: ")
 
-                elif opcion_select_mascotas == "2":
-                    os.system('cls')
-                    print("SELECT POR ID MASCOTAS")
-                    mascota_a_seleccionar = input("Ingrese mascotas a seleccionar")
-                    mascota = Mascota.SelectMascotaporID(mascota_a_seleccionar)
-                    print(str(mascota.id), mascota.nombre, mascota.tipo, mascota.dueno.id)
+                    if opcion_select_mascotas == "1":
+                        os.system('cls')
+                        print("SELECT ALL MASCOTAS")
+                        listamascotas = Mascota.SelectMascota()
+                        for item in listamascotas:
+                            print(item.id, item.nombre, item.tipo, item.dueno.id)
 
-                elif opcion_select_mascotas == "3":
-                    break
-                elif opcion_select_mascotas == "4":
-                    loop = False
-                else:   equivocacion_select_mascotas = input("Opcion incorrecta")
+                    elif opcion_select_mascotas == "2":
+                        os.system('cls')
+                        print("SELECT POR ID MASCOTAS")
+                        mascota_a_seleccionar = input("Ingrese mascotas a seleccionar")
+                        mascota = Mascota.SelectMascotaporID(mascota_a_seleccionar)
+                        print(str(mascota.id), mascota.nombre, mascota.tipo, mascota.dueno.id)
+
+                    elif opcion_select_mascotas == "3":
+                        break
+                    elif opcion_select_mascotas == "4":
+                        loop = False
+                    else:   equivocacion_select_mascotas = input("Opcion incorrecta")
             elif opcion_mascotas == "5":
                 break
             elif opcion_mascotas == "6":
@@ -284,8 +286,8 @@ while(loop):
                         os.system('cls')
                         print("SELECT POR ID CONSULTAS")
                         consulta_a_seleccionar = input("Ingrese la consulta a seleccionar")
-                        consulta = Consulta.SelectMascotaporID(consulta_a_seleccionar)
-                        print(str(consulta.id), consulta.precio, consulta.diagnostico, item.mascota.id, item.veterinario.id)
+                        consulta = Consulta.SelectConsultasPorID(consulta_a_seleccionar)
+                        print(str(consulta.id), consulta.precio, consulta.diagnostico, consulta.mascota.id, consulta.veterinario.id)
                     elif opcion_select_consultas == "3":
                         break
                     elif opcion_select_consultas == "4":
